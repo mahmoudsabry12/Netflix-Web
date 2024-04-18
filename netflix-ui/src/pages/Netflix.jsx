31afdee1bab4683383b3432fcae65238492371bd
Mahmoud Sabry <mahmoudsabry.udacity@gmail.com>
2024-04-18 06:24:53 +0200

    Finish Navbar and Logout

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
+
 
 export const Netflix = () => {
+  const [isScrolled,setIsScrolled] = useState(false)
+  window.onscroll = ()=>{
+    setIsScrolled(window.pageYOffset=0 ? false:true);
+    return ()=> (window.onscroll=null)
+  }
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
   )
 }
