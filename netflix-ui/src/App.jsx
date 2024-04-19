9c316284c63c3816ab17b6bd506b10da6e558f5d
Mahmoud Sabry <mahmoudsabry.udacity@gmail.com>
2024-04-19 10:19:18 +0200

    Home Page and Movie Player

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import { Login } from './pages/Login'
 import  Signup from './pages/Signup'
 import { Netflix } from './pages/Netflix'
+import Player from './pages/Player'
 function App() {
 
 
@@ -11,8 +12,10 @@ function App() {
      <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Netflix />}/>
+          <Route exact path='/player' element={<Player />}/>
           <Route exact path='/login' element={<Login />}/>
           <Route exact path='/signup' element={<Signup />}/>
+
         </Routes>
      </BrowserRouter>
     </>
